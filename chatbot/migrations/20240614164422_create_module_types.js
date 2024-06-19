bb92f7613f632538539d02fe796c25e6100fe164
Shubham <shubham@Shubhams-MacBook-Pro.local>
2024-06-19 19:32:45 +0530

    adding migrations

--- chatbot/migrations/20240614164422_create_module_types.js
+++ chatbot/migrations/20240614164422_create_module_types.js
@@ -1,11 +1,15 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('module_types', function(table) {
-      table.increments('id').primary();
-      table.string('name').notNullable();
-    });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTable('module_types');
-  };
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('module', function(table) {
+    table.increments('id').primary();
+    table.string('module_type', 50);
+    table.string('created_by', 100);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at');
+    table.timestamp('deleted_at');
+  });
+};
+
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('module');
+};
--- chatbot/migrations/20240614164451_create_questions.js
+++ chatbot/migrations/20240614164451_create_questions.js
@@ -1,25 +1,37 @@
-exports.up = function(knex) {
-    return knex.schema
-      .createTable('questions', function(table) {
-        table.increments('id').primary(); // Auto-incrementing primary key
-        table.integer('parent_id').unsigned().references('id').inTable('Fields').onDelete('CASCADE'); // Self-referencing foreign key
-        table.string('name', 255).notNullable();
-        table.string('label', 255);
-        table.string('type', 50);
-        table.string('placeholder', 255);
-        table.boolean('required');
-        table.json('options');
-      })
-      .createTable('answers', function(table) {
-        table.increments('id').primary(); // Auto-incrementing primary key
-        table.integer('field_id').unsigned().references('id').inTable('Fields').onDelete('CASCADE'); // Foreign key referencing Fields table
-        table.string('value', 255);
-      });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema
-      .dropTableIfExists('questions')
-      .dropTableIfExists('answers');
-  };
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('questions', function(table) {
+    table.increments('id').primary();
+    table.integer('module_id').unsigned();
+    table.foreign('module_id').references('module.id'); // Correct schema qualification
+    table.text('question_text');
+    table.text('order');
+    table.text('is_mandatory').defaultTo(false);;
+    table.string('name', 255).notNullable();
+    table.string('label', 255);
+    table.string('type', 50);
+    table.string('placeholder', 255);
+    // table.boolean('draft').defaultTo(true);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at');
+    table.timestamp('deleted_at');
+  });
+
+  await knex.schema.createTable('answers', function(table) {
+    table.increments('id').primary();
+    table.integer('question_id').unsigned();
+    table.foreign('question_id').references('questions.id'); // Correct schema qualification
+    table.text('answer_text');
+    table.boolean('is_correct');
+    table.date('date_created');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at');
+    table.timestamp('deleted_at');
+  });
+};
+
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('questions')
+    .dropTableIfExists('answers');
+};
