fe327479947c06cb1d4bd56f5315fb895511751e
Shubham <shubham@Shubhams-MacBook-Pro.local>
2024-06-21 18:55:01 +0530

    adding chatbot code

--- chatbot/migrations/20240614164422_create_module_types.js
+++ chatbot/migrations/20240614164422_create_module_types.js
@@ -2,7 +2,6 @@ exports.up = async function(knex) {
   await knex.schema.createTable('module', function(table) {
     table.increments('id').primary();
     table.string('module_type', 50);
-    table.string('created_by', 100);
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at');
     table.timestamp('deleted_at');
