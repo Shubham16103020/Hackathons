fbb25c533bad32ecb2567b94f5f05d5d705aa26f
Himanshu verma <himanshu.verma@dpworld.com>
2024-06-26 23:33:05 +0530

    migration + seeds

--- chatbot/seeds/01_module_seed.js
+++ chatbot/seeds/01_module_seed.js
@@ -4,9 +4,9 @@ exports.seed = async function(knex) {
       .then(async function () {
         // Inserts seed entries
         return await knex('configuration.modules').insert([
-          { id: 200, template_id: 100, module_type: 'Company Profile'},
-          { id: 201, template_id: 100, module_type: 'Company Details'},
-          { id: 202, template_id: 100, module_type: 'Company KYC Information'},
+          { id: 100, module_type: 'CREDIT FACILITY'},
+          { id: 101, module_type: 'FINANCE'},
+          { id: 102, module_type: 'ACCOUNTING'},
           // Add more seed data as needed
         ]);
       });
